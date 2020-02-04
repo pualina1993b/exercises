b5cef9b0d8af313420cb0c9855d312d8a8ce10d6
pualina1993b <pualina1993b@gmail.com>
2020-02-04 12:03:24 +0100

    added dictionary

--- file1.py
+++ file1.py
@@ -1,5 +1,6 @@
 import math
 
+
 def greet_user(first_name, last_name):
     print(f'Hi!!! {first_name} {last_name} !')
 
@@ -15,6 +16,25 @@ def countDistance2d(firstPlaceA, firstPlaceB, secondPlaceA, secondPlaceB):
     return math.sqrt(math.pow(deltaA, 2) + math.pow(deltaB, 2))
 
 
+elementy = [1, 2, 3]
+x, y, z = elementy
+print(f'trzecie miejsce to {z}')
+
+# dictionaries
+liczby = {
+    "1": "jeden",
+    "2": "dwa",
+    "3": "trzy",
+    "4": "cztery"
+}
+
+phone = input("phone: ")
+output = " "
+
+for ch in phone:
+    output += liczby.get(ch, "!") + " "
+
+print(output)
 # warm = input("is it warm? type yes/no  ")
 # # converting string to boolean
 # is_warm = "yes" in warm
