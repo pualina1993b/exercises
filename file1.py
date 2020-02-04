c6814a0805bb6cc859af775a3970da69ce4b78c9
pualina1993b <pualina1993b@gmail.com>
2020-02-04 11:33:36 +0100

    added file1.py new wersion

--- file1.py
+++ file1.py
@@ -1,19 +1,35 @@
-
-warm = input("is it warm? type yes/no  ")
-#converting string to boolean
-is_warm = "yes" in warm
-temp = int(input("Enter temperature  "))
-
-if temp > 19 and is_warm:
-    print("It's warm")
-else:
-    print("So it's cold")
+import math
 
 def greet_user(first_name, last_name):
     print(f'Hi!!! {first_name} {last_name} !')
 
 
-#2 line breaks after function
-print("Start")
-greet_user(input('type your first name  '), input('type your last name  '))
-print("Finish")
+def get_greet_user_text(first_name, last_name):
+    text = f'Hi!!! {first_name} {last_name} !'
+    return text
+
+
+def countDistance2d(firstPlaceA, firstPlaceB, secondPlaceA, secondPlaceB):
+    deltaA = secondPlaceA - firstPlaceA
+    deltaB = secondPlaceB - firstPlaceB
+    return math.sqrt(math.pow(deltaA, 2) + math.pow(deltaB, 2))
+
+
+# warm = input("is it warm? type yes/no  ")
+# # converting string to boolean
+# is_warm = "yes" in warm
+# temp = int(input("Enter temperature  "))
+#
+# if temp > 19 and is_warm:
+#     print("It's warm")
+# else:
+#     print("So it's cold")
+#
+# # 2 line breaks after function
+# print("Start")
+# greetText = get_greet_user_text(input('type your first name  '), input('type your last name  '))
+# print(greetText)
+# print("Finish")
+
+name = countDistance2d(0, 0, 2, 2)
+print(name)
